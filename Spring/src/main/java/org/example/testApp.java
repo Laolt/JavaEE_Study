61f5bb4dfc2273769ed4318d057794fd1019514b
FuHappy0327 <2108333068@qq.com>
2023-10-07 23:23:24 +0800

    测试

--- Spring/src/main/java/org/example/testApp.java
+++ Spring/src/main/java/org/example/testApp.java
@@ -8,7 +8,7 @@ public class testApp {
     public static void main(String[] args) {
         //获取IOC容器
         //ApplicationContext tac=new ClassPathXmlApplicationContext("testAppContext.xml.test");
-        ClassPathXmlApplicationContext tac=new ClassPathXmlApplicationContext("testAppContext.xml.test");
+        ClassPathXmlApplicationContext tac=new ClassPathXmlApplicationContext("testAppContext.xml");
         //获取bean
         //直接关闭容器tac.close();
         //registerShutdownHook()设置关闭钩子使得虚拟机关闭之前关闭容器
