29527076cf182dcf3fc2a12948bb996aec63ddbc
FuHappy0327 <2108333068@qq.com>
2023-10-08 11:30:05 +0800

    @海

--- javaEETest_SpringAndMybatisForAnnotation/src/main/java/org/example/testSpringAndMybatisForAnnotation.java
+++ javaEETest_SpringAndMybatisForAnnotation/src/main/java/org/example/testSpringAndMybatisForAnnotation.java
@@ -1,19 +1,13 @@
 package org.example;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.example.config.SpringConfig;
-import org.example.dao.testDaoMFA;
 import org.example.service.testServiceMFA;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.io.IOException;
 
-
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class testSpringAndMybatisForAnnotation {
+    public static void main(String[] args){
         ApplicationContext ac=new AnnotationConfigApplicationContext(SpringConfig.class);
         ac.getBean(testServiceMFA.class).selectAll();
     }
