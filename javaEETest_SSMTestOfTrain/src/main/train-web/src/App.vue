2b17b038733ea246e6952bf23982f949e7ad6ac5
FuHappy0327 <2108333068@qq.com>
2023-10-16 08:44:51 +0800

    目前进度SSM

---
+++ javaEETest_SSMTestOfTrain/src/main/train-web/src/App.vue
@@ -0,0 +1,26 @@
+<!-- 通过localhost:5173访问 -->
+<template>
+  <div @click="getTrain"></div>
+  <h1 v-for="(item,index) in dataList" >{{ item }}</h1>
+</template>
+<script>
+export default{
+  data(){
+    return{
+      msg:"666",
+      dataList:[]
+    }
+  },
+  methods(){
+    getTrain(){
+      axios.get("http://localhost:8080/trains/0").then((res)=>{
+        console.log(res.data)
+        this.dataList=res.data.data
+      });
+    }
+  }
+}
+</script>
+
+
+
