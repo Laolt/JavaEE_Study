7991c79a5ccde7b12fc8f25c1b96e8478ae8a950
FuHappy0327 <2108333068@qq.com>
2023-10-17 18:56:13 +0800

    目前进度springBoot

--- javaEETest_SSMTestOfTrain/src/main/java/org/example/config/SpringMvcConfig.java
+++ javaEETest_SSMTestOfTrain/src/main/java/org/example/config/SpringMvcConfig.java
@@ -1,11 +1,23 @@
 package org.example.config;
 
+import org.example.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan({"org.example.controller","org.example.config"})
 @EnableWebMvc
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
+//    //注册拦截器（）侵入式较强
+//    @Autowired
+//    private ProjectInterceptor projectInterceptor;
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(projectInterceptor).addPathPatterns("/trains/*","/trains");
+//    }
 }
