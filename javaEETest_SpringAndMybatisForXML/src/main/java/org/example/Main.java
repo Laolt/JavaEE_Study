c3040649bfd914897011e0990c54f9b558f8b595
FuHappy0327 <2108333068@qq.com>
2023-10-17 20:33:02 +0800

    目前进度：springBoot

--- javaEETest_SpringAndMybatisForXML/src/main/java/org/example/Main.java
+++ javaEETest_SpringAndMybatisForXML/src/main/java/org/example/Main.java
@@ -6,9 +6,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-
+//        创建spring容器
         ApplicationContext ctx=new ClassPathXmlApplicationContext("springconfig.xml");
+//        获取service层的bean
         testService ts=ctx.getBean("testService",testService.class);
+//        调用里面的方法
         ts.selectAll();
     }
 }
